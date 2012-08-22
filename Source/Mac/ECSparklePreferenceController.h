577aa09ed2c5580a1d8531a4bd176dd747a10b74
Sam Deane <sam@elegantchaos.com>
2012-08-22 12:18:55 +0100

    no really, got compiling this time

--- Source/Mac/ECSparklePreferenceController.h
+++ Source/Mac/ECSparklePreferenceController.h
@@ -5,8 +5,6 @@
 //  Copyright 2012 Sam Deane, Elegant Chaos. All rights reserved.
 // --------------------------------------------------------------------------
 
-#import "ECPreferencePanel.h"
-
 @interface ECSparklePreferenceController : ECPreferencePanel
 
 @property (strong, nonatomic) NSString* introText;
